3e2897e222b7f4b41f39d0f9f1dcf3221c24f8b2
Ivan Ho <38537881+hhkit@users.noreply.github.com>
2025-12-18 21:43:07 +0000

    e2e uses large sizes that actually gets vectorized

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -3,4 +3,5 @@ export SCRIPT_PATH=$(realpath $(dirname "$0"))
 export IREE_BUILD=$(realpath $SCRIPT_PATH/../build)
 cd $SCRIPT_PATH
 cmake --build $IREE_BUILD
-$IREE_BUILD/tools/iree-opt --pass-pipeline="builtin.module(func.func(iree-llvmcpu-tile{tiling-level=distribution},iree-linalg-ext-decompose-aggregated-ops{filter-ops=iree_linalg_ext.exp_reduction}))" $SCRIPT_PATH/dispatch.mlir > out.mlir
+# $IREE_BUILD/tools/iree-opt --pass-pipeline="builtin.module(func.func(iree-llvmcpu-tile{tiling-level=distribution},iree-linalg-ext-decompose-aggregated-ops{filter-ops=iree_linalg_ext.exp_reduction}))" $SCRIPT_PATH/dispatch.mlir > out.mlir
+$IREE_BUILD/tools/iree-compile --iree-hal-target-device=local --iree-hal-local-target-device-backends=llvm-cpu --iree-llvmcpu-target-cpu=host  --compile-from=flow --mlir-print-ir-after-all e2e.mlir 2>e2e.dbg.mlir 1>e2e.out.vmfb
